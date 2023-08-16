c3ca80553e385a2f3670e1de61eac2dd2f10a623
yssefnoam <2020noam@gmail.com>
2023-08-16 18:47:46 +0100

    done: ex0

--- day00/ex03/NULL_not_found.py
+++ day00/ex03/NULL_not_found.py
@@ -1,23 +1,17 @@
 def NULL_not_found(object: any)-> int:
-    # print(object)
-    # print(type(object))
     o = object.__class__
-    print("object: ", type(object))
 
-    if o is None:
-        print("Nothing: None", type(object))
-        return 0
-    elif o is float and object == float("NaN"):
-        print("Cheese: nan", type(object))
-        return 0
-    elif object is None:
-        print("Nothing: None", type(object))
-        return 0
-    elif object is None:
-        print("Nothing: None", type(object))
-        return 0
-    elif object is None:
-        print("Nothing: None", type(object))
+    if not object or object != object:
+        if o is float:
+            print("Cheese: nan", o)
+        elif o is int and object == 0:
+            print("Zero: 0 ", o)
+        elif o is str and object == '':
+            print("Empty: ", type(object))
+        elif object == False:
+            print("Fake: False", type(object))
+        else:
+            print("Nothing: None", o)
         return 0
     else:
         print("Type not Found")
